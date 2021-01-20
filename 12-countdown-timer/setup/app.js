3a9dc5620f06057dbbe650bfae470e93b4a0b446
Karen <kareninejapiassu@gmail.com>
2021-01-20 23:38:48 +0100

    Lorem Ipsum Project

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -31,9 +31,9 @@ let tempYear = tempDate.getFullYear();
 let tempMonth = tempDate.getMonth();
 let tempDay = tempDate.getDate();
 
-//let futureDate = new Date(2021,0,23,0,0,0);
+let futureDate = new Date(2021,7,1,0,0,0);
 
-const futureDate = new Date(tempYear,tempMonth,tempDay + 10,22, 30, 0);
+//const futureDate = new Date(tempYear,tempMonth,tempDay + 2,22, 30, 0);
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
 const minutes = futureDate.getMinutes();
